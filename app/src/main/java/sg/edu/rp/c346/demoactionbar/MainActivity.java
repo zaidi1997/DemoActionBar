08965913285c554742a6651cbb78fce3d0a825e3
zaidi1997 <17053109@myrp.edu.sg>
2019-07-16 10:47:18 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demoactionbar/MainActivity.java
@@ -0,0 +1,21 @@
+package sg.edu.rp.c346.demoactionbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+}
